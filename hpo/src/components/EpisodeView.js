d1000be0762f79369846c58e295af796f874ce84
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-12-14 12:29:56 -0800

    Add deployment test

--- hpo/src/components/EpisodeView.js
+++ hpo/src/components/EpisodeView.js
@@ -215,11 +215,12 @@ class EpisodeView extends Component {
 								)
 								.map((p, index) => (
 									<div key={index}>
-										<div
+										{/* <div
 											dangerouslySetInnerHTML={{
 												__html: p
 											}}
-										/>
+										/> */}
+										{p}
 										<br />
 									</div>
 								))}
